5e9a48cd162d9edca0a27b649b990c2f1d53ea35
rishabhjaiswal3 <rj838486@gmail.com>
2025-08-05 09:13:47 +0530

    added changes

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getProfile, saveProfile,getLeaderboard,checkNameExistance, saveName, getName,login } = require('../controllers/profileController');
 const verifyUser = require('../routes/middleware/verifyUser');
-router.get('/', getProfile);
+router.post('/game', getProfile);
 router.post('/', saveProfile);
 router.get('/leaderboard',getLeaderboard);
 router.post('/name', checkNameExistance);
