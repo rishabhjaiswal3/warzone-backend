387eca7b8750f0724abac2498f2050d634a2f272
rishabhjaiswal3 <rj838486@gmail.com>
2025-08-05 08:15:36 +0530

    added changes in backend

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ mongoose.connect(process.env.MONGO_URI, {
   .catch(err => console.error('MongoDB connection error:', err));
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3300;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
