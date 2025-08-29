fc04f4fc7a5c013223644a5b97494495de401ed4
bigclitphobia <sidhanth6am@gmail.com>
2025-08-29 12:26:55 +0700

    Update profileRoutes.js
    Router

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getProfile, saveProfile,getLeaderboard,checkNameExistance, getDailyQuests, saveName, getName,login } = require('../controllers/profileController');
+const { getProfile, saveProfile,getLeaderboard,checkNameExistance, getDailyQuests, getDailyQuestByType, saveName, getName,login } = require('../controllers/profileController');
 const verifyUser = require('../routes/middleware/verifyUser');
 router.get('/', getProfile);
 router.post('/', saveProfile);
 router.get('/dailyQuests', getDailyQuests);
+router.get('/dailyQuests/type/:type', getDailyQuestByType);
 router.get('/leaderboard',getLeaderboard);
 router.post('/name', checkNameExistance);
 router.post('/saveName',verifyUser, saveName);
