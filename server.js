69299512a61b1e98cbe640246d3c6aee26105d40
rishabhjaiswal3 <rj838486@gmail.com>
2025-11-23 13:40:08 +0530

    remove unnecessary changes

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const allowedOrigins = [
   'http://www.warzonewarriors.xyz',
   'http://warzonewarriors.xyz',
   'http://localhost:3000',
-  'http://localhost:3001'
+  'http://localhost:3001',
+  'https://warzonewarrior.vercel.app/'
 ];
 
 const corsOptions = {
